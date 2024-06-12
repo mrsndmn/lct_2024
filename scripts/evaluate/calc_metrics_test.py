57c7a16ec2881c7f5e17c0535a30cfb7efc92ecd
Your Name <you@example.com>
2024-06-12 08:42:26 +0000

    fix calc metrics test script

--- scripts/evaluate/calc_metrics_test.py
+++ scripts/evaluate/calc_metrics_test.py
@@ -16,25 +16,32 @@ def test_metrics_calc():
 
     interval_start_offset = df['interval_i'] * (1 * 16000)
     full_interval_dutation_samples = 16000 * 10
-    ending_padding_samples = (5 - 1) * 16000
+    ending_padding_samples = 5 * 16000
     df['interval_should_match'] = (df['augmented_audio_offset'] <  interval_start_offset) & (interval_start_offset < df['augmented_audio_offset'] + full_interval_dutation_samples - ending_padding_samples)
 
-    matched_predictions = df[df['interval_should_match'] & (df['hit_i'] == 0)]
+    matched_predictions = df # df[df['interval_should_match']]
     print("matched_predictions", len(matched_predictions))
 
-    current_youtube_id = matched_predictions.iloc[0]['youtube_id']
+    num_hits = (matched_predictions['hit_i'].max() + 1)
+    interval_seconds = len(df) // num_hits // len(uniq_youtube_ids)
 
-    num_samples = (df['hit_i'] == 0).sum()
-    score_table = np.zeros([ num_samples, len(uniq_youtube_ids) ])
+    num_samples = len(uniq_youtube_ids) * interval_seconds
+    
+    index_no_class = len(uniq_youtube_ids)
+
+    score_table = np.zeros([ num_samples, len(uniq_youtube_ids) + 1 ])
     target_score_table = np.zeros([ num_samples ])
     for i, (_, row) in enumerate(matched_predictions.iterrows()):
-        sample_i = i // 5
+        sample_i = i // num_hits
         row_youtube_id = row['youtube_id']
         
         youtube_id_index = youtube_id_to_index[row_youtube_id]
 
         if row['hit_i'] == 0:
-            target_score_table[sample_i] = youtube_id_index
+            if row['interval_should_match']:
+                target_score_table[sample_i] = youtube_id_index
+            else:
+                target_score_table[sample_i] = index_no_class
 
         # if score_table[sample_i, youtube_id_index] != 0:
         #     raise Exception("index is already filled [{sample_i}, {youtube_id_index}]")
