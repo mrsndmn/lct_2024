3c1d348323f1577898d7fd18f3628414d4fed2f3
Your Name <you@example.com>
2024-06-16 17:17:24 +0000

    add docs and script for data preloading

---
+++ scripts/data/download_demo_files.sh
@@ -0,0 +1,11 @@
+
+# Скачиваем индексы для аудио и видео
+wget https://storage.yandexcloud.net/lct2024-task-14/audio_index_embeddings.zip
+wget https://storage.yandexcloud.net/lct2024-task-14/video-index.zip
+
+# веса для аудио-модельки
+wget https://storage.yandexcloud.net/lct2024-task-14/electric-yogurt-97.zip
+
+# веса для визуальной модальности
+wget https://storage.yandexcloud.net/lct2024-task-14/ruby-moon-17.zip
+
